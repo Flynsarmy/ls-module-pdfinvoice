a9e8a33e4fc37e0299288767d4afd45102f6147d
flynsarmy <flynsarmy@gmail.com>
2014-09-05 11:36:15 +1000

    Add field comment

--- models/flynsarmypdfinvoice_configuration.php
+++ models/flynsarmypdfinvoice_configuration.php
@@ -12,7 +12,7 @@
 
 		protected function build_form()
 		{
-			$this->add_field('custom_css', 'Custom CSS', 'full', db_text)->tab('Appearance')->size('giant')->cssClasses('code')->language('css')->renderAs(frm_code_editor)->saveCallback('save_code');
+			$this->add_field('custom_css', 'Custom CSS', 'full', db_text)->tab('Appearance')->size('giant')->cssClasses('code')->language('css')->renderAs(frm_code_editor)->saveCallback('save_code')->comment("Add custom CSS here to change the way your invoice looks.", 'above');
 		}
 	}
 
